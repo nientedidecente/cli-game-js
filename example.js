abec85048b855a6100eb07daa18b8e64d1cec2c1
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-01-01 12:33:41 +0000

    introducing enemy moving towards player

--- example.js
+++ example.js
@@ -1,49 +1,76 @@
 var readlineSync = require("readline-sync");
 var Clear = require("clui").Clear;
+const range = require("uvk").range;
 var key = "";
-var row = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const NUMBER_OF_LINES = 40
+var row = range(NUMBER_OF_LINES).map(() => 0);
 var map = row.map(() => row);
 
-var position = { x: 0, y: 0 };
+var position = { x: NUMBER_OF_LINES / 2, y: NUMBER_OF_LINES / 2 };
+var enemyPosition = { x: 0, y: 0 };
 
 var actions = {
-  a: function(position) {
+  a: function (position) {
     position.x -= 1;
     position.x = Math.max(position.x, 0);
     return position;
   },
-  d: function(position) {
+  d: function (position) {
     position.x += 1;
     position.x = Math.min(position.x, row.length - 1);
     return position;
   },
-  w: function(position) {
+  w: function (position) {
     position.y -= 1;
     position.y = Math.max(position.y, 0);
     return position;
   },
-  s: function(position) {
+  s: function (position) {
     position.y += 1;
     position.y = Math.min(position.y, row.length - 1);
     return position;
   }
 };
 
-const printMatrix = (map, position) => {
+const printMatrix = (map, position, enemyPosition) => {
+
   map.forEach((row, i) => {
     var value = "";
     row.forEach((cell, j) => {
-      value += position.x === j && position.y === i ? " * " : "  ";
+      if (position.x === j && position.y === i) {
+        value += " * ";
+      } else if (enemyPosition.x === j && enemyPosition.y === i) {
+        value += " @ ";
+      } else {
+        value += "   ";
+      }
     });
-    console.log(value);
+    console.log(value + "|");
   });
+  console.log(range(NUMBER_OF_LINES).map(() => " _ ").join(''))
 };
+
+const calculateNextStep = (position, enemyPosition) => {
+  if (enemyPosition.x > position.x) {
+    enemyPosition.x -= 1;
+  } else if (enemyPosition.x < position.x) {
+    enemyPosition.x += 1;
+  }
+  if (enemyPosition.y > position.y) {
+    enemyPosition.y -= 1;
+  } else if (enemyPosition.y < position.y) {
+    enemyPosition.y += 1;
+  }
+  return enemyPosition;
+}
 
 while (key !== "q") {
   Clear();
   position = actions[key] ? actions[key](position) : position;
-  printMatrix(map, position);
-  console.log(position.x, position.y);
+  enemyPosition = calculateNextStep(position, enemyPosition);
+  printMatrix(map, position, enemyPosition);
+  console.log(position);
+  console.log(enemyPosition);
   key = readlineSync.keyIn("", {
     hideEchoBack: true,
     mask: "",
